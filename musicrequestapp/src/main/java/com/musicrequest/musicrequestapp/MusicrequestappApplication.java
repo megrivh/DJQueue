2bfaad85ce8de9043d0de362d4ad53eab555d347
Megan Rivera Hammock <mzr0142@auburn.edu>
2025-01-28 14:13:26 -0500

    Initial commit

---
+++ musicrequestapp/src/main/java/com/musicrequest/musicrequestapp/MusicrequestappApplication.java
@@ -0,0 +1,13 @@
+package com.musicrequest.musicrequestapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicrequestappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicrequestappApplication.class, args);
+	}
+
+}
---
+++ musicrequestapp/src/test/java/com/musicrequest/musicrequestapp/MusicrequestappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musicrequest.musicrequestapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicrequestappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
